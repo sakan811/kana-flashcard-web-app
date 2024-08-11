b174a664cff94b5740739ff8cb6be6b7011897bd
Sakan <sakanbeer@gmail.com>
2024-08-11 23:40:00 +0700

    v1.1.1 - Changed some JS and JSX to TS and TSX. - Refactored frontend codes - Use Vitest instead of Jest - Adjusted Docker compose file

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,7 @@ app.use(cors());
 // Connect to PostgreSQL
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST || 'localhost',
+  port: process.env.DB_PORT || 5432,
   dialect: 'postgres',
 });
 
