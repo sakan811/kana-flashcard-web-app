8e089b65f2ce0ff64c7303844f0994909b2835cd
Sakan <sakanbeer@gmail.com>
2024-08-12 02:07:40 +0700

    v1.1.9 - Changed some JS and JSX to TS and TSX. - Refactored frontend codes - Use Vitest instead of Jest - Adjusted Docker compose file - Added tests for Performance Table

--- backend/wait-for-db.sh
+++ backend/wait-for-db.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
-set -e
 
 host="$1"
 shift
-cmd="$@"
+cmd="$*"
 
 until pg_isready -h "$host"; do
-  >&2 echo "Postgres is unavailable - sleeping"
+  echo "Postgres is unavailable - sleeping"
   sleep 1
 done
 
->&2 echo "Postgres is up - executing command"
-exec $cmd
+echo "Postgres is up - executing command"
+exec "$cmd"
