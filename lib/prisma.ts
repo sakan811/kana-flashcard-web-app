b6b848929f59ab7a27093f7fa4df46d43fdcfef8
Sakan Nirattisaykul <sakan.nir@gmail.com>
2025-06-05 01:46:43 +0700

    feat: add user authentication models and update database schema

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,21 +1,21 @@
 /*
  * SakuMari - Japanese Kana Flashcard App
  * Copyright (C) 2025  Sakan Nirattisaykul
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published
  * by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
- * 
+ *
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-import { PrismaClient } from "../prisma/app/generated/prisma/client";
+import { PrismaClient } from "../src/generated/client";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
