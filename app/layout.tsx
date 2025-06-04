f9c213907ceac03b1d243119f9a041ed9b11ab90
Sakan Nirattisaykul <sakan.nir@gmail.com>
2025-06-05 01:47:07 +0700

    feat: enhance user experience with session handling and loading state in HomePage

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,24 @@
 /*
  * SakuMari - Japanese Kana Flashcard App
  * Copyright (C) 2025  Sakan Nirattisaykul
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published
  * by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
- * 
+ *
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 import { Metadata } from "next";
 import "./globals.css";
 import SourceCodeNotice from "@/components/SourceCodeNotice";
+import Providers from "@/components/SessionProviders";
 
 export const metadata: Metadata = {
   title: "Japanese Kana Flashcard App",
@@ -32,7 +33,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="min-h-screen bg-gradient-to-br from-[#fad182] via-[#f5c55a] to-[#fad182] font-sans antialiased">
-        {children}
+        <Providers>{children}</Providers>
         <SourceCodeNotice />
       </body>
     </html>
