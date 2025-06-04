18306d39eb975bacd0f66e80dfc41bb77e41b917
Sakan Nirattisaykul <sakan.nir@gmail.com>
2025-06-04 21:48:02 +0700

    feat: add NextAuth route handler and update dependencies for authentication

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,20 @@
+/*
+ * SakuMari - Japanese Kana Flashcard App
+ * Copyright (C) 2025  Sakan Nirattisaykul
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * 
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import { handlers } from "@/lib/auth"
+
+export const { GET, POST } = handlers
