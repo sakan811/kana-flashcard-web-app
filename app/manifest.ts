80d7745b711ef1921a6ee17de0b22d2ab21c2d98
Sakan <sakanbeer@gmail.com>
2025-06-05 16:01:52 +0700

    feat: add manifest, robot, and sitemap files for improved SEO and app metadata

---
+++ app/manifest.ts
@@ -0,0 +1,41 @@
+/*
+ * SakuMari - Japanese Kana Flashcard App
+ * Copyright (C) 2025  Sakan Nirattisaykul
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import type { MetadataRoute } from 'next'
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: 'SakuMari - Japanese Kana Flashcard App',
+    short_name: 'SakuMari',
+    description: 'Master Japanese Hiragana and Katakana with interactive flashcards and progress tracking',
+    start_url: '/',
+    display: 'standalone',
+    background_color: '#fad182',
+    theme_color: '#d1622b',
+    orientation: 'portrait',
+    scope: '/',
+    lang: 'en',
+    categories: ['education', 'productivity'],
+    icons: [
+      {
+        src: '/favicon.ico',
+        sizes: 'any',
+        type: 'image/x-icon',
+      },
+    ],
+  }
+}
