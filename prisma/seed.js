ecb88ef4d10c50ddfadab9725495514530030239
Sakan Nirattisaykul <94357278+sakan811@users.noreply.github.com>
2025-06-23 08:17:37 +0700

    Update seed.js

--- prisma/seed.js
+++ prisma/seed.js
@@ -15,7 +15,7 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-const { PrismaClient } = require("../node_modules/generated");
+const { PrismaClient } = require("@prisma/client");
 
 const prisma = new PrismaClient();
 
